a7339b080a3bf4bdc5edcf1ffd1e1020467d8501
pallavisnair <99475089+pallavisnair@users.noreply.github.com>
2022-02-17 17:52:42 +0100

    Update almost_there.py

--- src/almost_there.py
+++ src/almost_there.py
@@ -1,7 +1,7 @@
 def almost_there(num):
-    x = 100-num
-    y = 200-num
-    if x in range(0,11) or y in range(0,11):
+    x = abs(100-num)
+    y = abs(200-num)
+    if x <=10 or y<=10:
         return "True"
     else:
         return "False"
